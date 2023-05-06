1fd6f0ca71be7d194e49b12a3c8940a9f142288c
Tang Tao <784933671@qq.com>
2023-05-06 10:24:40 +0800

    重构部署目录

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,18 +2,15 @@ export default {
   lang: "zh-CN",
   title: "前端开发教程", //站点标题
   description: "前端开发教程", //mate标签description，多用于搜索引擎抓取摘要
-  base: "/course/", //部署站点的基础路径
-  outDir: "../course", //输出目录
   lastUpdated: true, //最后更新时间
   themeConfig: {
     siteTitle: "前端开发教程",
     logo: "/logo.png",
     nav: [
       { text: "主页", link: "/" },
-      { text: "vue相关", link: "/articles/组件库环境搭建" },
-      { text: "环境配置", link: "/circumstances/nvm" },
+      { text: "前端", link: "/articles/组件库环境搭建" },
       { text: "公司", link: "/company/svn" },
-      { text: "gitee", link: "https://gitee.com/geeksdidi" },
+      { text: "gitee", link: "https://gitee.com/t784933671" },
     ],
     sidebar: {
       "/articles/": [
@@ -25,7 +22,10 @@ export default {
               text: "组件库环境搭建",
               link: "/articles/组件库环境搭建",
             },
-            { text: "gulp的使用", link: "/articles/gulp的使用" },
+            {
+              text: "gulp的使用",
+              link: "/articles/gulp的使用"
+            },
           ],
         },
         {
@@ -38,6 +38,43 @@ export default {
               link: "/articles/pina和vuex",
             },
           ],
+        },
+        {
+          text: "npm管理",
+          collapsible: true,
+          collapsed: true,
+          items: [
+            {
+              text: "nvm实现多版本node自由切换",
+              link: "/articles/circumstances/nvm",
+            },
+            {
+              text: "yarn npm 设置淘宝镜像",
+              link: "/articles/circumstances/yarn_npm",
+            }
+          ],
+        },
+        {
+          text: "git",
+          collapsible: true,
+          collapsed: true,
+          items: [
+            {
+              text: "git常用命令",
+              link: "/articles/git/index",
+            }
+          ]
+        },
+        {
+          text: "脚本",
+          collapsible: true,
+          collapsed: true,
+          items: [
+            {
+              text: "windows删除文件夹下所有文件",
+              link: "/articles/command/index",
+            }
+          ]
         },
         {
           text: "其他",
@@ -59,36 +96,20 @@ export default {
           ],
         },
       ],
-      "/circumstances/": [
-        {
-          text: "npm管理",
-          collapsible: true,
-          collapsed: true,
-          items: [
-            {
-              text: "nvm实现多版本node自由切换",
-              link: "/circumstances/nvm",
-            },
-            {
-              text: "yarn npm 设置淘宝镜像",
-              link: "/circumstances/yarn npm 设置淘宝镜像",
-            },
-            {
-              text: "git常用命令",
-              link: "/circumstances/git常用命令",
-            },
-          ],
-        },
-      ],
+
       "/company/": [
         {
-          text: "svn管理",
+          text: "创立公司",
           collapsible: true,
           collapsed: true,
           items: [
             {
               text: "svn地址",
               link: "/company/svn",
+            },
+            {
+              text: "vpn相关",
+              link: "/company/vpn",
             },
           ],
         },
